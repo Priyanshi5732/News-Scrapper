49036b19df78c29bcb914e53515f545fce61aa47
Priyanshi <gs2113012@sgsitsindore.in>
2024-01-28 23:58:23 +0530

    Build a REST API for the project

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const request = require("request");
 const mysql = require("mysql2");
 const cronitor = require("cronitor")(process.env.Client_key);
 const cron = require("node-cron");
-const monitor = new cronitor.Monitor('process.env.Monitor_key');
-const bodyParser = require('body-parser');
-const express =require('express');
-const app= express();
+const monitor = new cronitor.Monitor("process.env.Monitor_key");
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -29,7 +29,6 @@ connection.connect((err) => {
   console.log("Connected to MySQL as id " + connection.threadId);
 });
 
-
 cron.schedule(
   //scraps everyday at midnight
   "0 0 * * *",
@@ -37,7 +36,7 @@ cron.schedule(
     let url = "https://www.sgsits.ac.in/index.php/exam";
     request(url, function (error, response, html) {
       // the job has started
-      monitor.ping({state: 'run'});
+      monitor.ping({ state: "run" });
       if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
         let website_title = $("td.list-title");
@@ -49,15 +48,14 @@ cron.schedule(
           connection.query(sql, [scrapedTitle], (err, results) => {
             if (err) {
               console.error("Error inserting data: " + err.stack);
-              monitor.ping({ state: 'fail' });
+              monitor.ping({ state: "fail" });
               return;
             }
             console.log(
               "Data inserted successfully. Inserted ID:",
               results.insertId
-              
             );
-            monitor.ping({state: 'complete'});
+            monitor.ping({ state: "complete" });
           });
         });
       }
@@ -70,30 +68,44 @@ cron.schedule(
 );
 
 //defined API endpoint to get new data by ID
-app.get('/api/titles/:id', (req,res)=>{
-  const newsId= req.params.id;
+app.get("/api/titles/:id", (req, res) => {
+  const newsId = req.params.id;
 
   //query the database to get data by ID
-  const sql= 'SELECT * FROM college_news where id=?'
+  const sql = "SELECT * FROM college_news where id=?";
   connection.query(sql, [newsId], (err, results) => {
     if (err) {
-        console.error('Error retrieving news data:', err);
-        res.status(500).json({ error: 'Internal Server Error' });
-        return;
+      console.error("Error retrieving news data:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
     }
 
     if (results.length === 0) {
-        res.status(404).json({ error: 'Title not found' });
+      res.status(404).json({ error: "Title not found" });
     } else {
-        res.json(results[0]); // Assuming the ID is unique,so return the first result
+      res.json(results[0]); // Assuming the ID is unique,so return the first result
     }
+  });
+});
+
+//defined API to get whole data
+app.get("/api/titles", (req, res) => {
+  const sql = "SELECT * FROM college_news";
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error("Error retrieving news data:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+
+    res.json(results); // Return all news data as a JSON array
   });
 });
 
 //good practice for initialising the port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 process.on("SIGINT", () => {
